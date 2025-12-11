3a1c0132ac89e0bf5c5792a8ed7c8488070345b2
ElreyInhumano <mqgareth@gmail.com>
2025-12-11 06:16:18 -0500

    Import TMPs
    Matías Quintanilla

--- Assets/Scripts/MainScene.cs
+++ Assets/Scripts/MainScene.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using GameJolt.UI;
 public class MainScene : MonoBehaviour
 {
     void Start()
     {
-        GameJoltUI.Instance.ShowSignIn();
+        GameJoltUI.Instance.ShowSignIn((success) =>
+        {
+            if (success)
+            {
+                SceneManager.LoadScene("LevelSelectorScene");
+            }
+            else
+            {
+                Debug.Log("Error");
+            }
+        });
     }
-
 }
